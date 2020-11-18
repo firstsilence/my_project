a669bca638fe42629ab285c9c5d6c0ad5f1e046e
Will <firstsilence@gmail.com>
2020-11-19 03:20:55 +0900

    반경내 마커표시 수정

--- static/thingsJs.js
+++ static/thingsJs.js
@@ -1,5 +1,16 @@
 var map;
 var locPosition;
+var arr = new Array();
+
+// var jsonLocation = '${ctx}/resource/json/test.json';
+$.getJSON('static/location.json', function(data){
+arr = $.each(data, function(I, item){
+// console.log(item.eqb_nm);
+
+        arr=[item.eqb_nm, item.lat, item.lng];
+
+});
+});
 
 function mapDraw() {    //지도 생
     var container = document.getElementById('map'); //지도를 담을 영역의 DOM 레퍼런스
@@ -17,8 +28,13 @@ function mapDraw() {    //지도 생
         // GeoLocation을 이용해서 접속 위치를 얻어옵니다
         navigator.geolocation.getCurrentPosition(function (position) {
 
-            var lat = position.coords.latitude, // 위도
-                lon = position.coords.longitude; // 경도
+            // var lat = position.coords.latitude, // 위도
+            //     lon = position.coords.longitude; // 경도
+
+            //강의실 위도,경도 (37.5605672, 126.94334859999998)
+            var lat = 37.5605672,
+                lon = 126.94334859999998;
+
             console.log("lat : " + lat);
             console.log("lon : " + lon);
             locPosition = new kakao.maps.LatLng(lat, lon); // 마커가 표시될 위치를 geolocation으로 얻어온 좌표로 생성합니다
@@ -98,25 +114,28 @@ function getLocation() {
 }
 
 // var radius=0;
-var markerTmp;      // 마커
-var polyLineTmp;    // 두지점간 직선거리
+// var markerTmp;      // 마커
+// var polyLineTmp;    // 두지점간 직선거리
 
-var arr = new Array();
-arr[0] = ["테스트1", 37.6397252, 126.671359, "대구 달서구 장기동 790"];
-arr[1] = ["테스트2", 35.8502186751, 128.516473546, "대구 달서구 장기동 162-1"];
-arr[2] = ["테스트3", 35.8507674215, 128.520114592, "대구 달서구 용산동 410-9"];
-arr[3] = ["테스트4", 35.8491570477, 128.528283511, "대구 달서구 용산동 215-9"];
-arr[4] = ["테스트5", 35.854902859257784, 128.5296955671568, "대구 달서구 용산동 955"];
+
+// arr[0] = ["테스트1", 37.6397252, 126.671359, "대구 달서구 장기동 790"];
+// arr[1] = ["테스트2", 35.8502186751, 128.516473546, "대구 달서구 장기동 162-1"];
+// arr[2] = ["테스트3", 35.8507674215, 128.520114592, "대구 달서구 용산동 410-9"];
+// arr[3] = ["테스트4", 35.8491570477, 128.528283511, "대구 달서구 용산동 215-9"];
+// arr[4] = ["테스트5", 35.854902859257784, 128.5296955671568, "대구 달서구 용산동 955"];
+
+// arr[0] = ["동인교회", 37.5723823, 127.0096855, "동인교회!!!"];
+// arr[1] = ["남부교회", 37.5790055, 127.0148552, "남부교회!!!"];
 
 function radiusCircle(locPosition) {
     // 원(Circle)의 옵션으로 넣어준 반지름
 
-    // var radius=document.getElementById('distance').value;
     mapDraw();
     var radius = 0;
-    radius = $("#distance").val();
+    radius = ($("#distance").val())*1000;
     alert("!!!" + radius);
     alert("radius locPosition : " + locPosition);
+    alert("&&&&arr val : "+arr[0][0]);
     var circle = new kakao.maps.Circle({
         map: map,
         center: locPosition,
@@ -128,42 +147,57 @@ function radiusCircle(locPosition) {
         fillColor: '#D3D5BF',
         fillOpacity: 0.5
     });
-    //  markerTmp = new kakao.maps.Marker({
-    //      position: new kakao.maps.LatLng(arr[i][1],arr[i][2]),
-    //      title: arr[i][0],
-    //      map:map
-    //  });
-    //
-    //
-    //
-    //  polyLineTmp = new kakao.maps.Polyline({
-    //      map: map,
-    //      path: [
-    //
-    //          locPosition, new kakao.maps.LatLng(arr[i][1],arr[i][2])
-    //      ],
-    //      strokeWeight: 2,
-    //      strokeColor: '#FF00FF',
-    // strokeOpacity: 0.8,
-    // strokeStyle: 'dashed'
-    //  });
+    for (var i=0;i<arr.length;i++) {
+       var markerTmp = new kakao.maps.Marker({
+            position: new kakao.maps.LatLng(arr[i][1], arr[i][2]),
+            // position: new kakao.maps.LatLng(arr[i][10], arr[i][6]),
+            title: arr[i][11],
+            map: map
+        });
 
 
-    // kakao.markers(function (m) {
-    //     var c1 = map.getCenter();
-    //     var c2 = m.getPosition();
-    //     var poly = new Polyline({
-    //         // map: map, 을 하지 않아도 거리는 구할 수 있다.
-    //         path: [c1, c2]
-    //     });
-    //     var dist = poly.getLength(); // m 단위로 리턴
-    //
-    //     if (dist < radius) {
-    //         m.setMap(map);
-    //     } else {
-    //         m.setMap(null);
-    //     }
-    // });
+       var polyLineTmp = new kakao.maps.Polyline({
+            map: map,
+            path: [
+
+                locPosition, new kakao.maps.LatLng(arr[i][1], arr[i][2])
+                // locPosition, new kakao.maps.LatLng(arr[i][10], arr[i][6])
+            ],
+            strokeWeight: 2,
+            strokeColor: '#FF00FF',
+            strokeOpacity: 0.8,
+            strokeStyle: 'dashed'
+        });
+    var distance = polyLineTmp.getLength(); //m단위 리턴
+    alert("distance val["+i+"]: "+distance);
+    if (distance > radius){
+        markerTmp.setMap(null);
+        polyLineTmp.setMap(null);
+        alert("11111");
+    }
+// alert("arr[0][0] : "+arr[0][0]);
+        // kakao.maps.Marker(function (m) {
+        //     var c1 = m.getCenter();
+        //     var c2 = m.getPosition();
+        //     var poly = new Polyline({
+        //         // map: map, 을 하지 않아도 거리는 구할 수 있다.
+        //         path: [c1, c2]
+        //     });
+        //     var dist = poly.getLength(); // m 단위로 리턴
+        //
+        //     if (dist < radius) {
+        //         m.setMap(map);
+        //         // console.log(dist);
+        //     } else {
+        //         m.setMap(null);
+        //         console.log(dist);
+        //     }
+        // });
+    }
+
 }
+
+
+
 
 
